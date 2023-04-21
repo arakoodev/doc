980b23d63df3350b44f3f0b001712f044ffec1ef
Sandeep Srinivasa <sss@lambdacurry.com>
2023-04-21 16:16:15 +0530

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/logo-arako.ico',
 
   // Set the production url of your site here
-  url: 'https://arakoodev.github.io',
+  url: 'https://www.arakoo.ai',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
