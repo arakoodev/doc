4523b32b7218d2d58736e334a9721840dc310047
Sandeep Srinivasa <sss@lambdacurry.com>
2023-05-06 18:26:37 +0530

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -243,7 +243,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Arakoo Project`,
       },
       prism: {
         theme: lightCodeTheme,
